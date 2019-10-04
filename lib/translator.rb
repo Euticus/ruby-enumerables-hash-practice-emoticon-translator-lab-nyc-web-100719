96770c6a0ac55fea83344c51269a24fa1b68af3b
Jay  Stewart <jayr.stewart@live.com>
2019-10-04 03:12:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,7 +27,7 @@ def get_japanese_emoticon(file_path, emoticon)
 end
 
 def get_english_meaning(file_path, emoticon)
-  data = load_library(file_path)[:get_emoticon][emoticon]
+  data = load_library(file_path)[:get_meaning][emoticon]
   if data
     return data
   else 
