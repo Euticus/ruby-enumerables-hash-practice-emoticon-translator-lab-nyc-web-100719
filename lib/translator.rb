6abaf67df7606d8c950ca1eae5530d6e2279a24f
Jay  Stewart <jayr.stewart@live.com>
2019-10-04 02:41:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,6 +22,7 @@ def get_japanese_emoticon(file_path, emoticon)
   data[:get_emoticon].each do |japanese, english|
       if emoticon == japanese
         return english
+        binding.pry
         # if japanese[-1] && japanese != emoticon
         #   return "Sorry that emoticon was not found"
       # else 
